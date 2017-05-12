c75bb3eb44eff0ae3ce7024b2648e62eedd36181
Maryna <mamukhina@luxoft.com>
2017-05-12 14:48:06 +0300

    6

--- src/Demo.java
+++ src/Demo.java
@@ -3,5 +3,8 @@ public class Demo {
 
     public static void main(String args[]) {
         System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
     }
 }
